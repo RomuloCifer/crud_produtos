100d47b26c834143116b6d7b0e360bdc627d47ec
Cif <ou_iy@hotmail.com>
2025-11-27 23:09:10 -0300

    Estava incomodado com a necessidade de cadastrar produto para ver se o programa estava funcionando.
    input basico para escolher a opção.

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ def main():
     print('=' * 5, "SISTEMA DE PRODUTOS", '=' * 5)
 
     #algo simples para começar pegando os inputs
-    nome = input("Digite o nome do produto: ")
-    preco = input("Digite o preço do produto: ")
-    qtd = input("Digite a quantidade do produto: ")
-    preco = float(preco)
-    qtd = int(qtd)
-    inserir_produto(nome, preco, qtd)
-    print("\n",nome, "cadastrado com sucesso")
+    while True:
+        escolha = input("1:INSERIR | 2:LISTAR | 3:SAIR")
+        if escolha == "1":
+            nome = input("Digite o nome do produto: ")
+            preco = input("Digite o preço do produto: ")
+            qtd = input("Digite a quantidade do produto: ")
+            preco = float(preco)
+            qtd = int(qtd)
+            inserir_produto(nome, preco, qtd)
+            print("\n",nome, "cadastrado com sucesso")
+        elif escolha == '2':
+            produtos = listar_produtos()
+            if not produtos:
+                print("Lista de produtos vazia.")
+            else:
+                for prod in produtos:
+                    prod_id, prod_nome, prod_preco, prod_qtd = prod
 
-    produtos = listar_produtos()
-    if not produtos:
-        print("Lista de produtos vazia.")
-    else:
-        for prod in produtos:
-            prod_id, prod_nome, prod_preco, prod_qtd = prod
-
-            print(f"ID:{prod_id} -> nome : {prod_nome} | preço : {prod_preco:.2f} | Quantidade : {prod_qtd}")
+                    print(f"ID:{prod_id} -> nome : {prod_nome} | preço : {prod_preco:.2f} | Quantidade : {prod_qtd}")
+        elif escolha == '3':
+            break
 
 if __name__ == "__main__":
     print(main())
