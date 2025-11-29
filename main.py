2b102132f062af4b64215fff22b984a0b77560da
Cif <ou_iy@hotmail.com>
2025-11-28 22:34:41 -0300

    Update ( O.K )

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
 from database import create_table
-from repository import inserir_produto, listar_produtos
+from repository import inserir_produto, listar_produtos, atualizar_produto
+
+
+def cadastrar_produto():
+    nome = input("Digite o nome do produto: ")
+    preco = input("Digite o preço do produto: ")
+    qtd = input("Digite a quantidade do produto: ")
+    preco = float(preco)
+    qtd = int(qtd)
+    inserir_produto(nome, preco, qtd)
+    print("\n",nome, "cadastrado com sucesso")
+
+def mostrar_produtos():
+    produtos = listar_produtos()
+    if not produtos:
+        print("Lista de produtos vazia.")
+    else:
+        for prod in produtos:
+            prod_id, prod_nome, prod_preco, prod_qtd = prod
+            print(f"ID:{prod_id} -> nome : {prod_nome} | preço : {prod_preco:.2f} | Quantidade : {prod_qtd}")
+
+def atualizar_produtos():
+    listar_produtos()
+    id_str = input("Qual ID do produto que deseja alterar?")
+    produto_id = int(id_str)
+
+    print("Digite os novos dados. \n")
+    novo_nome = input("Nome: ")
+    preco = input("Preço: ")
+    preco_float = float(preco)
+    quantidade = input("Quantidade")
+    qtd_int = int(quantidade)
+
+    sucesso = atualizar_produto(produto_id, novo_nome, preco_float, qtd_int)
+    if sucesso:
+        print("Item atualizado com sucesso!")
+    else:
+        print("Nenhum produto atualizado, cheque o ID")
+
+def mostrar_menu():
+    """Exibe o menu principal"""
+    print("\n=== Sistema de Produtos ===")
+    print("1 - Cadastrar produto")
+    print("2 - Listar produtos")
+    print("3 - Atualizar produto")
+    print("0 - Sair")
 
 def main():
     create_table()
@@ -7,26 +52,19 @@ def main():
 
     #algo simples para começar pegando os inputs
     while True:
-        escolha = input("1:INSERIR | 2:LISTAR | 3:SAIR")
-        if escolha == "1":
-            nome = input("Digite o nome do produto: ")
-            preco = input("Digite o preço do produto: ")
-            qtd = input("Digite a quantidade do produto: ")
-            preco = float(preco)
-            qtd = int(qtd)
-            inserir_produto(nome, preco, qtd)
-            print("\n",nome, "cadastrado com sucesso")
-        elif escolha == '2':
-            produtos = listar_produtos()
-            if not produtos:
-                print("Lista de produtos vazia.")
-            else:
-                for prod in produtos:
-                    prod_id, prod_nome, prod_preco, prod_qtd = prod
-
-                    print(f"ID:{prod_id} -> nome : {prod_nome} | preço : {prod_preco:.2f} | Quantidade : {prod_qtd}")
-        elif escolha == '3':
+        mostrar_menu()
+        opcao = input("Digite uma operação -> ")
+        if opcao == "1":
+            cadastrar_produto()
+        elif opcao == "2":
+            mostrar_produtos()
+        elif opcao == "3":
+            atualizar_produtos()
+        elif opcao == "0":
+            print("Saindo do sistema...")
             break
+        else:
+            print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     print(main())
--- repository.py
+++ repository.py
@@ -27,11 +27,24 @@ def listar_produtos():
             SELECT id, nome, preco, quantidade
             FROM produtos
             ORDER BY id"""
-    
     cursor.execute(sql)
-
     produtos = cursor.fetchall()
+    conn.close()
+    return produtos
 
+
+def atualizar_produto(produto_id: int, nome:str, preco:float, quantidade:int):
+    """Atualiza um produto já existente"""
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    sql = """
+        UPDATE produtos SET nome = ?, preco = ?, quantidade = ?
+        WHERE id = ?
+        """
+    cursor.execute(sql, (nome, preco, quantidade, produto_id))
+    conn.commit()
+    linhas_afetadas = cursor.rowcount # quantas linhas foram alteradas
     conn.close()
 
-    return produtos
+    return linhas_afetadas > 0
