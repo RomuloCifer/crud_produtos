a57bbcee6799d98ddb1d6648f810311786e3b5cd
Cif <ou_iy@hotmail.com>
2025-11-30 21:01:23 -0300

    feat: funções para leitura de float/int com tratamento de erros no cadastro/atualização dos produtos.

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from database import create_table
 from repository import inserir_produto, listar_produtos, atualizar_produto, deletar_produto
 
+def ler_float(mensagem: str) -> float:
+    """Lê um número float do usuário, tratando erros de entrada"""
+    while True:
+        valor_str = input(mensagem)
+        valor_str = valor_str.replace(",", ".")  # Substitui vírgula por ponto
+        try:
+            valor_float = float(valor_str)
+
+            return valor_float
+        except ValueError:
+            print("Valor inválido. Por favor, digite um número válido.")
+
+def ler_int(mensagem: str) -> int:  
+    """Lê um número inteiro, tratndo erros."""
+    while True:
+        valor_str = input(mensagem)
+        valor_str = valor_str.replace(",", ".")
+        try:
+            valor_int = int(valor_str)
+            return valor_int
+        except ValueError:
+            print("Valor inválido. Por favor, digite um número inteiro válido.")
 
 def cadastrar_produto():
+    """Cadastra um novo produto no sistema"""
     nome = input("Digite o nome do produto: ")
-    preco = input("Digite o preço do produto: ")
-    qtd = input("Digite a quantidade do produto: ")
-    preco = float(preco)
-    qtd = int(qtd)
+    #Usando as funções para garantir valores corretos.
+    preco = ler_float("Digite o preço do produto: ")
+    qtd = ler_int("Digite a quantidade do produto: ")
+
     inserir_produto(nome, preco, qtd)
     print("\n",nome, "cadastrado com sucesso")
 
@@ -21,33 +44,30 @@ def mostrar_produtos():
             print(f"ID:{prod_id} -> nome : {prod_nome} | preço : {prod_preco:.2f} | Quantidade : {prod_qtd}")
 
 def atualizar_produtos():
-    listar_produtos()
-    id_str = input("Qual ID do produto que deseja alterar?")
-    produto_id = int(id_str)
+    mostrar_produtos()
 
+    produto_id = ler_int("ID: ")
     print("Digite os novos dados. \n")
     novo_nome = input("Nome: ")
-    preco = input("Preço: ")
-    preco_float = float(preco)
-    quantidade = input("Quantidade")
-    qtd_int = int(quantidade)
+    preco = ler_float("Preço: ")
+    quantidade = ler_int("Quantidade: ")
 
-    sucesso = atualizar_produto(produto_id, novo_nome, preco_float, qtd_int)
+    sucesso = atualizar_produto(produto_id, novo_nome, preco, quantidade)
     if sucesso:
         print("Item atualizado com sucesso!")
     else:
         print("Nenhum produto atualizado, cheque o ID")
+
 def deletar_produtos_flow():
     print("\n=== Excluir produto ===")
     mostrar_produtos()
 
-    id_str = input("Digite o ID do produto que deseja deletar.")
-    id_int = int(id_str)
-    confirmacao = input(f"Tem certeza que deseja deletar o produto ID {id_int}? s/n")
+    produto_id = ler_int("ID: ")
+    confirmacao = input(f"Tem certeza que deseja deletar o produto ID {produto_id}? s/n")
     if confirmacao.lower() != "s":
         print("Operação cancelada..")
         return
-    sucesso = deletar_produto(id_int)
+    sucesso = deletar_produto(produto_id)
     if sucesso:
         print("Produto deletado com sucesso")
     else:
